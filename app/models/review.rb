b13258286c337aea6b7e6005a3ea69c8573e8938
Marta Nery <martarcnery@gmail.com>
2020-10-06 21:55:37 +0100

    Still doing it

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :content, length: { minimum: 1 }
+  validates :rating, length: { minimum: 1 }
+  validates :rating, numericality: { only_integer: true }
+  validates_numericality_of :rating, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 5
+end
